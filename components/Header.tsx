a985f56a79bdf48e8901f4039bd5ffb7a1fb1eed
unknown <deeppatel2@academic.rrc.ca>
2024-11-07 16:07:51 -0600

    Added authentication

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -13,6 +14,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const AuthModal = useAuthModal();
   const router = useRouter();
 
   const handelLogout = () => {
@@ -52,14 +54,14 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           <>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={AuthModal.onOpen}
                 className="bg-transparent text-neutral-300 font-medium"
               >
                 Sign Up
               </Button>
             </div>
             <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">
+              <Button onClick={AuthModal.onOpen} className="bg-white px-6 py-2">
                 Log in
               </Button>
             </div>
