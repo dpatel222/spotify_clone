da468df77a7e1dec8e581be35993b10c13f96845
unknown <deeppatel2@academic.rrc.ca>
2024-11-07 10:10:17 -0600

    Made a Header and Button Component and added design for login and sign up button

--- components/Library.tsx
+++ components/Library.tsx
@@ -14,8 +14,13 @@ const Library = () => {
           <TbPlaylist size={26} className="text-neutral-400" />
           <p className="text-neutral-400 font-medium text-md">Your Library</p>
         </div>
-        <AiOutlinePlus />
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
       </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">List Of Songs</div>
     </div>
   );
 };
